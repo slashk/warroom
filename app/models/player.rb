6b14f6aa7c87a39bb9c32bccb6cdb3d9d9716732
slashk <norcalmtbracing@gmail.com>
2009-02-22 09:34:37 -0800

    filling out player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,12 @@
 class Player < ActiveRecord::Base
+  has_one :pick
+  validates_numericality_of :yahoo_ref
+  validates_presence_of :player, :pos
+
+  # named scopes
+  # player by franchise
+  # player by pos
+  # player by team
+  named_scope :undrafted, :joins => 'LEFT JOIN picks ON players.id=picks.player_id', :include => [:pick]
+
 end
