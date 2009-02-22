ac2922068ca158cdf0df98f4c26d7774a8afb4cb
slashk <norcalmtbracing@gmail.com>
2009-02-22 11:44:07 -0800

    added draft_started? to whether draft is live or not

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,4 +12,9 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+  def draft_started?
+    Picks.count > 0
+  end
+
 end
