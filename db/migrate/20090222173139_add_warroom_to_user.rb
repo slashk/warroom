f68067049c6ce136c80dd0e6239d7a0f397b7f72
slashk <norcalmtbracing@gmail.com>
2009-02-22 09:44:26 -0800

    added team data into user schema

--- db/migrate/20090222173139_add_warroom_to_user.rb
+++ db/migrate/20090222173139_add_warroom_to_user.rb
@@ -2,10 +2,18 @@ class AddWarroomToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :team_id, :integer
     add_column :users, :role, :string
+
+    # team data
+    add_column :users, :draft_order, :integer, :default => 1
+    add_column :users, :team, :string
+
   end
 
   def self.down
     remove_column :users, :role
     remove_column :users, :team_id
+
+    remove_column :users, :draft_order
+    remove_column :users, :team
   end
 end
