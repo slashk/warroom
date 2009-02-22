79bc04231b9d16a369518eb5971215cca9e5357d
slashk <norcalmtbracing@gmail.com>
2009-02-22 15:44:49 -0800

    made user adding functional

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def draft_started?
+    false
+  end
+
+  def is_admin?
+    true
+  end
+
 end
