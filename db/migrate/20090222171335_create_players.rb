2281b6b5b13c4fd702ce559b5c99679b4f114b06
slashk <norcalmtbracing@gmail.com>
2009-02-22 09:29:15 -0800

    added player schema to migration

--- db/migrate/20090222171335_create_players.rb
+++ db/migrate/20090222171335_create_players.rb
@@ -1,6 +1,39 @@
 class CreatePlayers < ActiveRecord::Migration
   def self.up
-    create_table :players do |t|
+    create_table :players, :options => 'default charset=utf8' do |t|
+      t.integer :yahoo_ref,  :default => 0
+      t.string :player
+      t.string :team
+      t.string :pos
+      t.string :status
+      t.integer :rank, :default => 1300
+      t.real :IP
+      t.integer :W
+      t.integer :SV
+      t.integer :K
+      t.real :ERA
+      t.real :WHIP
+      t.integer :R
+      t.integer :HR
+      t.integer :RBI
+      t.integer :SB
+      t.real :AVG
+      t.integer :orank
+      t.integer :AB
+      t.integer :prank
+      t.integer :depth
+      t.string :note
+      t.integer :pW
+      t.integer :pSV
+      t.integer :pK
+      t.real :pERA
+      t.real :pWHIP
+      t.integer :pR
+      t.integer :pHR
+      t.integer :pRBI
+      t.integer :pSB
+      t.real :pAVG
+
 
       t.timestamps
     end
