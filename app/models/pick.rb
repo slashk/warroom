92c247b344da5f247c944962d35c7eab53ecf8c5
slashk <norcalmtbracing@gmail.com>
2009-03-28 14:07:59 -0700

    rewored error message

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -2,7 +2,7 @@ class Pick < ActiveRecord::Base
   belongs_to :player
   belongs_to :user
   validates_uniqueness_of :player_id, :message => "has already been drafted"
-  validates_uniqueness_of :pick_number, :message => "was already been taken"
+  validates_uniqueness_of :pick_number, :message => "was already taken"
   validates_numericality_of :player_id
   validates_numericality_of :user_id
 end
