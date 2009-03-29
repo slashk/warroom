b8506513bc7a17432e6b6444bae80dad0894b672
slashk <norcalmtbracing@gmail.com>
2009-03-29 13:40:04 -0700

    first functional commit of retainee

--- app/controllers/retainees_controller.rb
+++ app/controllers/retainees_controller.rb
@@ -1,44 +1,42 @@
 class RetaineesController < ApplicationController
-  # GET /retainees
-  # GET /retainees.xml
+  before_filter :login_required
+
   def index
-    @retainees = Retainee.find(:all)
+    @retainees = Retainee.find(:all, :order => :user_id, :include => [:player, :user])
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @retainees }
     end
   end
 
-  # GET /retainees/1
-  # GET /retainees/1.xml
   def show
     @retainee = Retainee.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @retainee }
     end
   end
 
-  # GET /retainees/new
-  # GET /retainees/new.xml
   def new
     @retainee = Retainee.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @retainee }
     end
   end
 
-  # GET /retainees/1/edit
   def edit
-    @retainee = Retainee.find(params[:id])
+    @user = User.find(current_user)
+    @players = Player.find_all_by_previousTeam(@user)
+    r = Retainee.find_all_by_user_id(@user)
+    if r.nitems > 0
+      rplayers = r.map {|x| x.player_id}
+      @retainees = @players.map {|x| x if rplayers.include?(x.id) }
+    else
+      @retainees = []
+    end
   end
 
-  # POST /retainees
-  # POST /retainees.xml
   def create
     @retainee = Retainee.new(params[:retainee])
 
@@ -46,16 +44,12 @@ class RetaineesController < ApplicationController
       if @retainee.save
         flash[:notice] = 'Retainee was successfully created.'
         format.html { redirect_to(@retainee) }
-        format.xml  { render :xml => @retainee, :status => :created, :location => @retainee }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @retainee.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /retainees/1
-  # PUT /retainees/1.xml
   def update
     @retainee = Retainee.find(params[:id])
 
@@ -63,23 +57,54 @@ class RetaineesController < ApplicationController
       if @retainee.update_attributes(params[:retainee])
         flash[:notice] = 'Retainee was successfully updated.'
         format.html { redirect_to(@retainee) }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @retainee.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /retainees/1
-  # DELETE /retainees/1.xml
   def destroy
     @retainee = Retainee.find(params[:id])
     @retainee.destroy
 
     respond_to do |format|
       format.html { redirect_to(retainees_url) }
-      format.xml  { head :ok }
     end
   end
+
+  def add_player_to_retainee_list
+    if check_compliance(current_user, params[:id])
+      @retainee = Retainee.new(:player_id => params[:id], :user_id => current_user)
+      if @retainee.save
+        @players = Player.find(params[:id])
+      else
+        @players = "Error"
+      end
+    end
+  end
+
+  def remove_player_from_retainee_list
+    @retainee = Retainee.find_by_player_id(params[:id])
+    @retainee.destroy
+    @id = params[:id]
+  end
+  
+  private
+
+  def check_compliance(user, newPlayer)
+    compliant = true
+    player = Player.find(newPlayer)
+    retainees = Retainee.find_all_by_user_id(user, :include => :player)
+    if player.pos.match(/P/)
+      pitchers = retainees.map {|x| x if x.player.pos.match(/P/)}
+      compliant = false if pitchers.size > 2
+    else
+      batters = retainees.map {|x| x if !x.player.pos.match(/P/)}
+      compliant = false if batters.size > 2
+    end
+    # check RP <= 2
+    # check OF <= 2
+    return compliant
+  end
+
 end
