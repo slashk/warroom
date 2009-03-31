2b45369287c7c4c500ff13e547317ad3364a8194
slashk <norcalmtbracing@gmail.com>
2009-03-31 15:48:41 -0700

    fixed huge logic / sql bug in named_scope :undrafted that wasn't removing drafted players from the list

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,7 +5,7 @@ class Player < ActiveRecord::Base
 
   # named scopes
   named_scope :byrank, {:order => "orank asc", :limit => 150}
-  named_scope :undrafted, :joins => 'LEFT JOIN picks ON players.id=picks.player_id' #, :include => [:pick]
+  named_scope :undrafted, :joins => 'LEFT JOIN picks ON players.id=picks.player_id', :conditions => "picks.id IS NULL"
   named_scope :byname, lambda {|name| {:conditions => ["player LIKE ?", "%#{name}%"]}}
   named_scope :bypos, lambda {|pos| {:conditions => ["pos like ?", "%#{pos}%"]}}
   named_scope :batters, {:conditions => ["pos not like ?", "%P%"]}
