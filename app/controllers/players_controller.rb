6475a713a109449e6fcac90dcb1a736804f8286e
slashk <norcalmtbracing@gmail.com>
2010-02-23 13:26:23 -0800

    error check players>index when no players loaded

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,27 +5,22 @@ class PlayersController < ApplicationController
   # GET /players.xml
   def index
     @players = Player.undrafted.byrank
-    @teams = User.draftorder
-    @watchlist = compile_watchlist(current_user)
-    # draft results
-    @picks = Pick.picks_taken
-    # Pick order
-    current_pick = find_current_pick
-    @upcoming = Pick.find(:all, :conditions => "pick_number >= #{current_pick.pick_number}",
-      :order => "pick_number asc", :limit => 10, :include => :user )
-    @last_pick_time =  draft_started? ? find_last_pick_time : Time.now
-    # my team
-    mypicks = Pick.find_all_by_user_id(current_user.id, :include => :player)
-    @myTeamCount = countPlayers(mypicks)
-    # @SS = myTeamCount['SS']
-    # @B1 = myTeamCount['1B']
-    # @B2 = myTeamCount['2B']
-    # @B3 = myTeamCount['3B']
-    # @OF = myTeamCount['OF']
-    # @SP = myTeamCount['SP']
-    # @RP = myTeamCount['RP']
-    # @C = myTeamCount['C']
-    # @P = myTeamCount['P']
+    unless @players.nil?
+      @teams = User.draftorder
+      @watchlist = compile_watchlist(current_user)
+      # draft results
+      @picks = Pick.picks_taken
+      # Pick order
+      current_pick = find_current_pick
+      @upcoming = Pick.find(:all, :conditions => "pick_number >= #{current_pick.pick_number}",
+        :order => "pick_number asc", :limit => 10, :include => :user )
+      @last_pick_time =  draft_started? ? find_last_pick_time : Time.now
+      # my team
+      mypicks = Pick.find_all_by_user_id(current_user.id, :include => :player)
+      @myTeamCount = countPlayers(mypicks)
+    else
+      redirect_to :controller => "admin"
+    end
   end
 
   # GET /players/1
