9a37210afb2d826cef52e6c74c873d795165c95c
/k <norcalmtbracing@gmail.com>
2010-03-17 16:33:38 -0700

    substantial js changes: many small functions to grab webservices, fleshed out playerLoop (except drafting function).

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,9 @@
 // javascript code for WarRoom
 
 // global variables
-var $currentPick = 0;
+var $currentPick = {"created_at":"2010-03-16T10:26:37-07:00","updated_at":"2010-03-16T10:26:37-07:00","player_id":null,"id":0,"user_id":0,"pick_number":0};
 var $interval = 10000;
+var $isItMyPick = "0";
 
 // All pages
 $(document).ready(function(){
@@ -14,16 +15,26 @@ $(document).ready(function(){
 
 function playerLoop() {
 	setInterval(function(){ 
-		$.get("/currentpick", function(){
-			// if dbCurrentPick > currentPick
-			//		update all sidebar widgets
-			// 		check isItMyPick?
-			//			redAlert -- make background colors red
-			//			pop alert that you are on clock
-			//			refresh screen
-			//			add + button to each player name
-			alert(response)
-		}, "json")
+		getCurrentPick();
+		refreshWatchlist();
+		refreshDraftList();
+		if ($isItMyPick) {
+			soundRedAlert(); // make background red to alert user
+			addDraftButton();
+		} else {
+			removeRedAlert();
+			removeDraftButton();
+		};
+		refreshMyTeam();
+		refreshComingNext();
+		// if dbCurrentPick > currentPick
+		//		update all sidebar widgets
+		// 		check isItMyPick?
+		//			redAlert -- make background colors red
+		//			pop alert that you are on clock
+		//			refresh screen
+		//			add + button to each player name
+		// setInterval(function(){ $("#myteam").load("/picks/myteam").highlight() }, $interval);
 		}, $interval);
 }
 
@@ -33,6 +44,42 @@ function playerTable(){
         "bJQueryUI": true,
         "sPaginationType": "full_numbers"
     });
+}
+
+function soundRedAlert() {
+	$("#sidebar").addClass("on-the-clock");
+	$("div#asshead").addClass("on-the-clock");
+}
+
+function removeRedAlert() {
+	$("#sidebar").removeClass("on-the-clock");	
+	$("div#asshead").addClass("on-the-clock");
+}
+
+function addDraftButton(){
+	
+}
+
+function removeDraftButton(){
+	
+}
+
+function getCurrentPick(){
+	 // sets a Pick object to $currentPick 
+	$.getJSON( '/currentpick', function( json ) {
+		if (json.pick.pick_number > $currentPick.pick_number) { 
+			$currentPick = json.pick;
+			refreshComingNext();
+			isItMyPick();
+		};	
+	});
+}
+
+function isItMyPick() {
+	// returns 1 or 0, which is true or false 
+	x = $.get('/isitmypick', function(data) {
+		$isItMyTurn = data;
+	});	
 }
 
 function setRetaineeDragAndDrop(){
@@ -66,20 +113,24 @@ function setRetaineeDragAndDrop(){
 	})
 }
 
-function refreshWatchlist($interval) {
-	setInterval(function(){ $("#watchlist").load("/watchlist/show/"+$.cookie("auth_token")).highlight() }, $interval);
+function refreshWatchlist() {
+	$("#watchlist").load("/watchlist/show/"+$.cookie("auth_token")).highlight();
 }
 
 function refreshMyTeam() {
-
+	$("#myteam").load("/myteam").highlight();
 }
 
 function refreshComingNext() {
-	
+	$("#inline").load("/inline").highlight();	
 }
 
 function refreshMyRetaineeList () {
 	$("#retainedPlayers").load("/retainees/retained_players/1").highlight();
+}
+
+function refreshDraftList() {
+	$("#draft_live").load("/draftpicks").highlight();	
 }
 
 function getPlayerId (x) {
