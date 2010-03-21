5a1482dba22b6a4f539b629adc7d37c90dfad654
/k <norcalmtbracing@gmail.com>
2010-03-21 13:09:51 -0700

    added pick tests for fixtures

--- test/unit/pick_test.rb
+++ test/unit/pick_test.rb
@@ -1,17 +1,19 @@
 require 'test_helper'
 
 class PickTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  #  def draft_started?
-  #  def draft_over?
-  #  def find_current_pick
-  # if this is the last pick of the draft, return the last pick of the draft
-  # find the team who is currently on the clock
-  #  def find_user_on_clock
-  # find the time of the last pick
-  #  def find_last_pick_time
 
-  test "the truth" do
-    assert true
+  # this set all assumes that draft is fully over (via fixtures)
+  test "should get number of taken" do
+    assert_equal(Pick.count, Pick.taken.count)
   end
+
+  test "should get number remaining" do
+    assert_equal(0, Pick.remaining.count)    
+  end
+
+  test "should return curent pick" do
+    # this returns zero because draft is over
+    assert_equal(0, Pick.current.count)
+  end
+
 end
