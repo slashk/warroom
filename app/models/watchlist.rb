b38b77de0b640726c853fe77656e0a7c3d7594c7
/k <norcalmtbracing@gmail.com>
2010-03-28 15:45:46 -0700

    added sort to watchlist named_scope

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -2,6 +2,11 @@ class Watchlist < ActiveRecord::Base
   belongs_to :player
   belongs_to :user
 
-  named_scope :mine, lambda {|user| {:joins => 'LEFT JOIN picks ON watchlists.player_id=picks.player_id', :conditions => ["watchlists.user_id = ? AND picks.player_id IS NULL",user], :include => [:player, :user]}}
+  named_scope :mine, lambda {|user| {
+    :joins => 'LEFT JOIN picks ON watchlists.player_id=picks.player_id', 
+    :conditions => ["watchlists.user_id = ? AND picks.player_id IS NULL",user],
+    :order => "watchlists.id desc", 
+    :include => [:player, :user]}
+    }
   
 end
