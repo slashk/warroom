3439e727e9380cf04b075752b4c9f02e181fc08d
/k <norcalmtbracing@gmail.com>
2010-04-02 15:09:14 -0700

    added tests for admin drafts

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -8,5 +8,29 @@ class AdminControllerTest < ActionController::TestCase
     assert_response :success, @response.body
   end
 
+  test "should get new" do
+    login_as :commish
+    get :new
+    assert_response :success, @response.body
+  end
+  
+  test "should create draft straight" do
+    login_as :commish
+    post :create, :rounds => "10", :copy_retainee => false, :drop_draft => true, :draft_type => "straight"
+    assert_redirected_to :action => :index
+    assert_equal(80, Pick.count)
+    assert_equal(User.all(:order => "draft_order asc").first.id, Pick.first(:order => "pick_number asc").user_id)
+    assert_equal(User.all(:order => "draft_order asc").first.id, Pick.find_by_pick_number(User.count+1).user_id)
+  end  
+
+  test "should create draft snake" do
+    login_as :commish
+    post :create, :rounds => "10", :copy_retainee => false, :drop_draft => true, :draft_type => "snake"
+    assert_redirected_to :action => :index
+    # assert_equal(80, Pick.count)  NEED TO RESEARCH THIS
+    assert_equal(User.all(:order => "draft_order asc").first.id, Pick.first(:order => "pick_number asc").user_id)
+    assert_equal(User.all(:order => "draft_order asc").last.id, Pick.find_by_pick_number(User.count+1).user_id)
+  end  
+
 
 end
