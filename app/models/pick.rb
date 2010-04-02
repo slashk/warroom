edf519bb9f377cf3672b5fa9b0e0f7aa573252d9
/k <norcalmtbracing@gmail.com>
2010-04-02 13:34:08 -0700

    workaround for bug in rails 2.3.5 around named_scopes and count

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -4,9 +4,31 @@ class Pick < ActiveRecord::Base
   validates_uniqueness_of :pick_number, :message => "was already taken"
   validates_numericality_of :user_id
 
-  named_scope :taken, :conditions => "player_id IS NOT NULL", :order => "pick_number desc", :include => [:player, :user]
-  named_scope :taken_limited, lambda {|x| {:conditions => "player_id IS NOT NULL", :order => "pick_number desc", :include => [:player, :user], :limit => x}}
-  named_scope :remaining, :conditions => "player_id IS NULL", :order => "pick_number asc", :include =>  :user
-  named_scope :current, :conditions => "player_id IS NULL", :order => 'pick_number asc', :limit => 1
+  named_scope :taken, 
+    :conditions => "player_id IS NOT NULL", 
+    :order => "pick_number desc", 
+    :include => [:player, :user]
+  named_scope :remaining, 
+    :conditions => "player_id IS NULL", 
+    :order => "pick_number asc", 
+    :include =>  :user
+  named_scope :current, 
+        :conditions => "player_id IS NULL", 
+        :order => 'pick_number asc', 
+        :limit => 1
+  
+  # these named scopes don't work correctly due to a Rails bug in 2.3.5
+  # named_scope :taken_limited, 
+  #     :conditions => "player_id IS NOT NULL", 
+  #     :order => "pick_number desc", 
+  #     :include => [:player, :user], 
+  #     :limit => 10
+  # named_scope :remaining_limited, 
+  #     :conditions => "player_id IS NULL", 
+  #     :order => "pick_number asc", 
+  #     :include =>  :user, 
+  #     :limit => 10
+  # named_scope :limited, 
+  #     :limit => 10
   
 end
--- test/unit/pick_test.rb
+++ test/unit/pick_test.rb
@@ -7,13 +7,32 @@ class PickTest < ActiveSupport::TestCase
     assert_equal(Pick.count, Pick.taken.count)
   end
 
+  test "should get number of taken with limit" do
+    assert_equal(10, Pick.all(:conditions => "player_id IS NOT NULL", :order => "pick_number asc", :include =>  :user, :limit => 10).size)
+  end
+
   test "should get number remaining" do
     assert_equal(0, Pick.remaining.count)    
+  end
+
+  test "should get number remaining when not zero" do
+    assert_difference "Pick.remaining.count", 15 do
+      y = Pick.all(:limit => 15)
+      y.each do |x|
+        x.player_id = nil
+        x.save
+      end
+    end
+    x = Pick.all(:conditions => "player_id IS NULL", 
+    :order => "pick_number asc", 
+    :include =>  :user, 
+    :limit => 10)
+    assert_equal(10, x.size)
   end
 
   test "should return curent pick" do
     # this returns zero because draft is over
     assert_equal(0, Pick.current.count)
   end
-
+  
 end
