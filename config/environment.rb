73d3e0122d32cd78b1a147b2db3711a566ea7ce6
/k <norcalmtbracing@gmail.com>
2010-09-07 21:04:59 -0700

    upgraded to rails 2.3.9 and datatables 1.7.1

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   #config.gem "less"
+  config.gem 'test-unit', :version => '>=2.0.7', :lib => false
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
+require 'test/unit'
 # use TURN if available http://github.com/TwP/turn
-begin; require 'turn'; rescue LoadError; end
+# begin; require 'turn'; rescue LoadError; end
 
 class ActiveSupport::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
